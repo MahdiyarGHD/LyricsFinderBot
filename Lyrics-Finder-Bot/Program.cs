930c4414b6b0a2b639c0a530d758f6080e698352
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-03-11 20:55:59 +0330

    Added project files

---
+++ Lyrics-Finder-Bot/Program.cs
@@ -0,0 +1,47 @@
+﻿using Lyrics_Finder_Bot.Controllers;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.SimpleCommandHandler.Logics;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("BOT_TOKEN");
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = [] 
+};
+
+CommandHandler commandHandler = new();
+
+commandHandler.RegisterCommandHandler(command: "/start", parameters: "%s\n%s", handler: MainCommandsController.StartCommand);
+
+botClient.StartReceiving(
+    updateHandler: commandHandler.Resolve,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
